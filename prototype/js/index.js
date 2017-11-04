c2a8dd4a23a23c1f794af993c86a49f9c12a4ae6
mirdathebirda <mirandachiu13@gmail.com>
2017-11-04 19:11:54 -0400

    mouse draws in the right place ;w;

--- prototype/js/index.js
+++ prototype/js/index.js
@@ -1,12 +1,6 @@
- var canvas, ctx, flag = false,
-     prevX = 0,
-     currX = 0,
-     prevY = 0,
-     currY = 0,
-     dot_flag = false;
+ var canvas, ctx, flag = false, prevX = 0, currX = 0, prevY = 0, currY = 0, dot_flag = false;
 
- var x = "black",
-     y = 2;
+ var color = "black", lineWidth = 2;
 
  function init() {
      canvas = document.getElementById("can");
@@ -26,6 +20,7 @@
      canvas.addEventListener("mouseout", function (e) {
          findxy("out", e)
      }, false);
+
  }
 
  function color(obj) {
@@ -61,8 +56,8 @@
      ctx.beginPath();
      ctx.moveTo(prevX, prevY);
      ctx.lineTo(currX, currY);
-     ctx.strokeStyle = x;
-     ctx.lineWidth = y;
+     ctx.strokeStyle = color;
+     ctx.lineWidth = lineWidth;
      ctx.stroke();
      ctx.closePath();
  }
@@ -75,19 +70,21 @@
      }
  }
 
- function save() {
-     document.getElementById("canvasimg").style.border = "2px solid";
-     var dataURL = canvas.toDataURL();
-     document.getElementById("canvasimg").src = dataURL;
-     document.getElementById("canvasimg").style.display = "inline";
- }
+ // function save() {
+ //     document.getElementById("canvasimg").style.border = "2px solid";
+ //     var dataURL = canvas.toDataURL();
+ //     document.getElementById("canvasimg").src = dataURL;
+ //     document.getElementById("canvasimg").style.display = "inline";
+ // }
 
  function findxy(res, e) {
+   var rect = canvas.getBoundingClientRect();
+
      if (res == "down") {
          prevX = currX;
          prevY = currY;
-         currX = e.clientX - canvas.offsetLeft;
-         currY = e.clientY - canvas.offsetTop;
+         currX = (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+         currY = (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
 
          flag = true;
          dot_flag = true;
@@ -106,8 +103,8 @@
          if (flag) {
              prevX = currX;
              prevY = currY;
-             currX = e.clientX - canvas.offsetLeft;
-             currY = e.clientY - canvas.offsetTop;
+             currX = (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+             currY = (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
              draw();
          }
      }
