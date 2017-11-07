9994376abb30cf33ebae71f7748f0d169e29b9ba
mirdathebirda <mirandachiu13@gmail.com>
2017-11-06 23:45:55 -0500

    submenu text

--- prototype/js/clippyController.js
+++ prototype/js/clippyController.js
@@ -12,13 +12,22 @@ class ClippyController extends Controller {
       var select = document.getElementById("select");
       var fill = document.getElementById("fill");
 
+      var mi = document.getElementsByClassName("menu-item");
+
       var dialogueText = document.getElementById("dialogueText");
 
       pencil.addEventListener("click", function (e) {
+        setTimeout(function(){
           dialogueText.innerHTML = "Annoying Pencil Event Text"
+        },5000);
       }, false);
       text.addEventListener("click", function (e) {
-            dialogueText.innerHTML = "Annoying Text Event Text"
+        setTimeout(function(){
+          dialogueText.innerHTML = "Annoying Text Event Text"
+        },5000);
+      }, false);
+      mi[8].addEventListener("click", function (e) {
+          dialogueText.innerHTML = "Annoying SubMenu Event Text"
       }, false);
     }
 }
