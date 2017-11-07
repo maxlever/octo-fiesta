8bec1c6046830d98d4eca60c9613b594710c348a
mirdathebirda <mirandachiu13@gmail.com>
2017-11-06 23:34:02 -0500

    text changes on click

--- prototype/js/clippyController.js
+++ prototype/js/clippyController.js
@@ -4,31 +4,21 @@ class ClippyController extends Controller {
         super(view, model);
     }
 
-    // var pencil = document.getElementById("pencil");
-    // var eraser = document.getElementById("eraser");
-    // var text = document.getElementById("text");
-    // var line = document.getElementById("line");
-    // var select = document.getElementById("select");
-    // var fill = document.getElementById("fill");
-    //
-    // notify() {
-    //   pencil.addEventListener("click", function (e) {
-    //       findxy("move", e)
-    //   }, false);
-    //   eraser.addEventListener("click", function (e) {
-    //       findxy("move", e)
-    //   }, false);
-    //   text.addEventListener("click", function (e) {
-    //       findxy("move", e)
-    //   }, false);
-    //   line.addEventListener("click", function (e) {
-    //       findxy("move", e)
-    //   }, false);
-    //   select.addEventListener("click", function (e) {
-    //       findxy("move", e)
-    //   }, false);
-    //   fill.addEventListener("click", function (e) {
-    //       findxy("move", e)
-    //   }, false);
-    // }
+    notify() {
+      var pencil = document.getElementById("pencil");
+      var eraser = document.getElementById("eraser");
+      var text = document.getElementById("text");
+      var line = document.getElementById("line");
+      var select = document.getElementById("select");
+      var fill = document.getElementById("fill");
+
+      var dialogueText = document.getElementById("dialogueText");
+
+      pencil.addEventListener("click", function (e) {
+          dialogueText.innerHTML = "Annoying Pencil Event Text"
+      }, false);
+      text.addEventListener("click", function (e) {
+            dialogueText.innerHTML = "Annoying Text Event Text"
+      }, false);
+    }
 }
