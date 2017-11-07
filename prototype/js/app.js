ebe07dc3c4156a1e246964ebfe4f4487ae447267
Max Lever <maxdavidlever@gmail.com>
2017-11-06 22:35:04 -0500

    add clippy to app

--- prototype/js/app.js
+++ prototype/js/app.js
@@ -3,17 +3,21 @@ const model = new Model(),
     toolbarView = new ToolbarView(model),
     footerView = new FooterView(model),
     canvasView = new CanvasView(model),
+    clippyView = new ClippyView(model),
     headerController = new HeaderController(headerView, model),
     toolbarController = new ToolbarController(toolbarView, model),
     footerController = new FooterController(footerView, model),
-    canvasController = new CanvasController(canvasView, model);
+    canvasController = new CanvasController(canvasView, model),
+    clippyController = new ClippyController(clippyView, model);
 
 model.register(
     headerView,
     toolbarView,
     footerView,
     canvasView,
+    clippyView,
     headerController,
     toolbarController,
     footerController,
-    canvasController);
+    canvasController,
+    clippyController);
