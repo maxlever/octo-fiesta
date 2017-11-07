825f41c36289f3159e24c4a4e9a89e4964596b73
JillianBurns <jillian.jb.burns@gmail.com>
2017-11-07 11:14:32 -0500

    Clippy move tool text

--- prototype/js/clippyController.js
+++ prototype/js/clippyController.js
@@ -11,6 +11,7 @@ class ClippyController extends Controller {
       var line = document.getElementById("line");
       var select = document.getElementById("select");
       var fill = document.getElementById("fill");
+      var fill = document.getElementById("move");
 
       var mi = document.getElementsByClassName("menu-item");
 
@@ -21,7 +22,7 @@ class ClippyController extends Controller {
           dialogueText.innerHTML = "Oh no! That's not how a pencil should act. In a well-designed "
           + "interface, tools should match their functions in the real world. That synchronicity "
           + "allows users to easily understand the functions of the interface."
-        },5000);
+        },3000);
       }, false);
 
       text.addEventListener("click", function (e) {
@@ -29,21 +30,22 @@ class ClippyController extends Controller {
           dialogueText.innerHTML = "Oh no! This text is no longer editable. In a well-designed "
           + "interface, users are able to easily undo their previous actions. This gives users "
           + "more control over their interactions, and reduces the time spent re-doing rather than un-doing."
-        },5000);
+        },3000);
       }, false);
-      
-      mi[8].addEventListener("click", function (e) {
+
+      mi[9].addEventListener("click", function (e) {
           dialogueText.innerHTML = "Oh no! That sure was a lot of menus. In a well-designed "
           + "interface, common actions are quick and often have shortcuts. This allows users to be "
           + "more efficient."
       }, false);
 
-      /* 
-        Clippy Move Tool Text
-        "Oh no! That tool is hidden out of sight inside a submenu. A well-designed interface "
-        + "displays necessary tools where users can see them. This reduces the user’s memory "
+      move.addEventListener("click", function (e) {
+        dialogueText.innerHTML = "Oh no! That tool is hidden out of sight inside a submenu. A well-designed interface "
+        + "displays necessary tools where users can see them. This reduces the user's memory "
         + "load, and allows them to learn the interface more easily."
+      }, false);
 
+      /* 
         Clippy Submenu Text
         "Hm..another menu."
         "Wow, still more menus?"
