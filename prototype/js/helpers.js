aab3f1cc00d0b7652f75506b469701ee1a6c3fb3
Max Lever <maxdavidlever@gmail.com>
2017-11-07 00:16:23 -0500

    remove some helpers

--- prototype/js/helpers.js
+++ prototype/js/helpers.js
@@ -2,35 +2,4 @@
 // queue
 function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
 
-function checkCondition(v1, operator, v2) {
-    switch(operator) {
-        case '==':
-            return (v1 == v2);
-        case '===':
-            return (v1 === v2);
-        case '!==':
-            return (v1 !== v2);
-        case '<':
-            return (v1 < v2);
-        case '<=':
-            return (v1 <= v2);
-        case '>':
-            return (v1 > v2);
-        case '>=':
-            return (v1 >= v2);
-        case '&&':
-            return (v1 && v2);
-        case '||':
-            return (v1 || v2);
-        default:
-            return false;
-    }
-}
-
-Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-    return checkCondition(v1, operator, v2)
-                ? options.fn(this)
-                : options.inverse(this);
-});
-
 Handlebars.registerPartial("menu", $( "#menu-partial" ).html() );
