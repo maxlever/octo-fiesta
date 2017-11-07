0c6ce558eb2e2846239137a4eb9f706e8e3a5138
mirdathebirda <mirandachiu13@gmail.com>
2017-11-06 23:17:42 -0500

    stub clippy text

--- prototype/js/clippyController.js
+++ prototype/js/clippyController.js
@@ -4,8 +4,31 @@ class ClippyController extends Controller {
         super(view, model);
     }
 
-
-    notify() {
-        // add listeners here
-    }
+    // var pencil = document.getElementById("pencil");
+    // var eraser = document.getElementById("eraser");
+    // var text = document.getElementById("text");
+    // var line = document.getElementById("line");
+    // var select = document.getElementById("select");
+    // var fill = document.getElementById("fill");
+    //
+    // notify() {
+    //   pencil.addEventListener("click", function (e) {
+    //       findxy("move", e)
+    //   }, false);
+    //   eraser.addEventListener("click", function (e) {
+    //       findxy("move", e)
+    //   }, false);
+    //   text.addEventListener("click", function (e) {
+    //       findxy("move", e)
+    //   }, false);
+    //   line.addEventListener("click", function (e) {
+    //       findxy("move", e)
+    //   }, false);
+    //   select.addEventListener("click", function (e) {
+    //       findxy("move", e)
+    //   }, false);
+    //   fill.addEventListener("click", function (e) {
+    //       findxy("move", e)
+    //   }, false);
+    // }
 }
